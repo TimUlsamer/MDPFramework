f8dd4718e9feb9629c5c46777185b3e9020a06f5
TimUlsamer <tim.ulsamer@web.de>
2025-07-18 00:24:05 +0200

    fixed local functions not being serializable by introducing class

--- mdp_framework/generators/continuous_generator.py
+++ mdp_framework/generators/continuous_generator.py
@@ -3,46 +3,36 @@
 import numpy as np
 from mdp_framework.core.continuous import ContinuousMDP
 
-def random_linear_dynamics(state_dim, action_dim, noise_std=0.01):
-    """
-    Erstellt eine lineare Zufallsdynamikfunktion für ContinuousMDP.
-    """
-    A = np.random.randn(state_dim, state_dim)
-    B = np.random.randn(state_dim, action_dim)
+class RandomLinearDynamics:
+    def __init__(self, state_dim, action_dim, noise_std=0.01):
+        self.A = np.random.randn(state_dim, state_dim)
+        self.B = np.random.randn(state_dim, action_dim)
+        self.noise_std = noise_std
+        self.state_dim = state_dim
 
-    def dynamics_func(state, action):
+    def __call__(self, state, action):
         state = np.asarray(state)
         action = np.asarray(action)
-        noise = np.random.randn(state_dim) * noise_std
-        return A @ state + B @ action + noise
+        noise = np.random.randn(self.state_dim) * self.noise_std
+        return self.A @ state + self.B @ action + noise
 
-    return dynamics_func
+class RandomRewardFunction:
+    def __init__(self, state_dim, action_dim):
+        self.w_s = np.random.randn(state_dim)
+        self.w_a = np.random.randn(action_dim)
 
-def random_reward_func(state_dim, action_dim):
-    """
-    Erstellt eine lineare Zufalls-Rewardfunktion für ContinuousMDP.
-    """
-    w_s = np.random.randn(state_dim)
-    w_a = np.random.randn(action_dim)
-
-    def reward_func(state, action):
+    def __call__(self, state, action):
         state = np.asarray(state)
         action = np.asarray(action)
-        return float(w_s @ state + w_a @ action)
-
-    return reward_func
+        return float(self.w_s @ state + self.w_a @ action)
 
 def random_continuous_mdp(
-    state_dim,
-    action_dim,
-    gamma=0.99,
-    noise_std=0.01
-):
-    """
-    Erstellt ein zufälliges stetiges MDP mit linearer Dynamik und linearer Rewardfunktion.
-    """
-    dynamics_func = random_linear_dynamics(state_dim, action_dim, noise_std=noise_std)
-    reward_func = random_reward_func(state_dim, action_dim)
+        state_dim,
+        action_dim,
+        gamma=0.99,
+        noise_std=0.01):
+    dynamics_func = RandomLinearDynamics(state_dim, action_dim, noise_std=noise_std)
+    reward_func = RandomRewardFunction(state_dim, action_dim)
     mdp = ContinuousMDP(state_dim, action_dim, dynamics_func, reward_func, gamma)
     return mdp
 
